b74a3938c50d560c938a99f1f228dbdd27219ec4
Ben Peachey <potherca@gmail.com>
2020-12-24 21:25:50 +0100

    Fix bug in font-switcher caused by not checking if content has already loaded.

--- assets/js/font-switcher.js
+++ assets/js/font-switcher.js
@@ -1,26 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const defaultFont = 'lora'
-  const searchParams = new URLSearchParams(window.location.search)
+(_ => {
+  const onloadHandler = _ => {
+    const defaultFont = 'lora'
+    const searchParams = new URLSearchParams(window.location.search)
 
-  if (searchParams.has('font')) {
-    const bodyClassList = document.body.classList;
-    const font = searchParams.get('font');
-    const fontSwitcher = document.querySelector('.font-switcher')
-    const SwitcherClassList = fontSwitcher.classList;
+    if (searchParams.has('font')) {
+      const bodyClassList = document.body.classList;
+      const font = searchParams.get('font');
+      const fontSwitcher = document.querySelector('.font-switcher')
+      const SwitcherClassList = fontSwitcher.classList;
 
-    SwitcherClassList.remove(font)
-    SwitcherClassList.add(defaultFont)
+      SwitcherClassList.remove(font)
+      SwitcherClassList.add(defaultFont)
 
-    fontSwitcher.value = ''
-    fontSwitcher.innerText = 'Regular Edition'
+      fontSwitcher.value = ''
+      fontSwitcher.innerText = 'Regular Edition'
 
-    bodyClassList.add(font)
-    bodyClassList.remove(defaultFont)
+      bodyClassList.add(font)
+      bodyClassList.remove(defaultFont)
 
-    document.querySelectorAll('a[href]').forEach(link => {
-      if (link.href.toLowerCase().includes('gunk') && ! link.href.includes(`font=${font}`)) {
-        link.href += `?font=${font}`
-      }
-    })
+      document.querySelectorAll('a[href]').forEach(link => {
+        if (link.href.toLowerCase().includes('gunk') && ! link.href.includes(`font=${font}`)) {
+          link.href += `?font=${font}`
+        }
+      })
+    }
   }
-})
+
+  if (document.readyState === "complete" || document.readyState === "loaded") {
+    onloadHandler()
+  } else {
+    window.addEventListener("DOMContentLoaded", onloadHandler);
+  }
+})();
